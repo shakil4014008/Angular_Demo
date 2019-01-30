98bcf491e136c65dde9348beb744d8a4d38d78bc
shakil4014008 <shakil4014008@gmail.com>
2019-01-29 21:36:58 -0500

    Angular ngFor trackBy

--- Angular2Demo/src/app/employee/employeelist.component.js
+++ Angular2Demo/src/app/employee/employeelist.component.js
@@ -5,23 +5,39 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var EmployeeListComponent = /** @class */ (function () {
     function EmployeeListComponent() {
         this.employees = [
-            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
-            { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
+            { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
             { code: 'emp103', name: 'Wom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
             { code: 'emp104', name: 'Xom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' }
         ];
     }
+    EmployeeListComponent.prototype.getEmployees = function () {
+        this.employees = [
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
+            { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '0`/6/1988' },
+            { code: 'emp103', name: 'Wom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp104', name: 'Xom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp105', name: 'Zom', gender: 'FeMale', annualSalary: 5500, dateofbirth: '25/6/1999' }
+        ];
+    };
+    EmployeeListComponent.prototype.trackByEmpCode = function (index, employee) {
+        return employee.code;
+    };
     EmployeeListComponent = __decorate([
         core_1.Component({
             selector: 'listEmployee',
             templateUrl: 'app/employee/employeeList.component.html',
             styleUrls: ['app/employee/employeeList.component.css']
-        })
+        }),
+        __metadata("design:paramtypes", [])
     ], EmployeeListComponent);
     return EmployeeListComponent;
 }());
--- Angular2Demo/src/app/employee/employeelist.component.ts
+++ Angular2Demo/src/app/employee/employeelist.component.ts
@@ -5,16 +5,28 @@
     templateUrl: 'app/employee/employeeList.component.html',
     styleUrls: ['app/employee/employeeList.component.css']
 })
-
-
-
-
 export class EmployeeListComponent {
+    employees: any[];
+    constructor()
+    {
+         this.employees= [
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
+            { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
+            { code: 'emp103', name: 'Wom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp104', name: 'Xom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' }
+        ];
+    }
 
-    employees: any[] = [
-        {code: 'emp101', name:'Tom', gender:'Male', annualSalary:5500, dateofbirth: '25/6/1988'}, 
-        { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988'}, 
-        { code: 'emp103', name: 'Wom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988'}, 
-        { code: 'emp104', name: 'Xom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988'} 
-    ]
+    getEmployees() {
+        this.employees = [
+            { code: 'emp101', name: 'Tom', gender: 'Male', annualSalary: 5500, dateofbirth: '01/6/1988' },
+            { code: 'emp102', name: 'Uom', gender: 'Male', annualSalary: 5500, dateofbirth: '0`/6/1988' },
+            { code: 'emp103', name: 'Wom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp104', name: 'Xom', gender: 'Male', annualSalary: 5500, dateofbirth: '25/6/1988' },
+            { code: 'emp105', name: 'Zom', gender: 'FeMale', annualSalary: 5500, dateofbirth: '25/6/1999' }
+        ];
+    }
+    trackByEmpCode(index: number, employee: any): string {
+        return employee.code;
+    }
 }
