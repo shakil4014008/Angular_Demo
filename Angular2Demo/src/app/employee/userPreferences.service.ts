ba60964e9e267d1ef1cdafcee4481422c69bd12b
shakil4014008 <shakil4014008@gmail.com>
2019-02-01 14:44:07 -0500

    Angular injection in appRootModule

---
+++ Angular2Demo/src/app/employee/userPreferences.service.ts
@@ -0,0 +1,11 @@
+﻿import { Injectable } from '@angular/core';
+
+
+@Injectable()
+export class UserPreferencesService {
+
+    constructor() {
+        console.log('New Instance of Service Created');
+    }
+    colourPreference: string = 'orange';
+}
--- Angular2Demo/src/app/home/home.component.ts
+++ Angular2Demo/src/app/home/home.component.ts
@@ -1,9 +1,29 @@
-﻿import {Component } from '@angular/core';
+﻿import { Component } from '@angular/core';
+import {UserPreferencesService} from '../employee/userPreferences.service';
 
 @Component({
-    template:`<h1>This is the home page</h1>`
+    template:`<h1>This is the home page</h1>
+            <div>
+                Colour Preference :
+                <input type='text' [(ngModel)]='colour' [style.background]="colour"/>
+            </div>`
 })
 
 export class HomeComponent {
+ 
+    constructor(private _userPreferencesService: UserPreferencesService) {
+       
+    }
 
+    // Implement a getter to retrieve the colourPreference value
+    // from the service
+    get colour(): string {
+        return this._userPreferencesService.colourPreference;
+    }
+
+    // Implement a setter to change the colourPreference value
+    // of the service
+    set colour(value: string) {
+        this._userPreferencesService.colourPreference = value;
+    }
 }
